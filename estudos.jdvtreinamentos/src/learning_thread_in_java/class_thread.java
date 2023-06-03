e9af34744986b749ec60d7b1fcaa5d72c599363d
João Paulo <joaopaulo.arteagle17@gmail.com>
2023-06-03 12:36:20 -0300

    Utilizando o thread sleep para pausar o tempo por execução

--- estudos.jdvtreinamentos/src/learning_thread_in_java/class_thread.java
+++ estudos.jdvtreinamentos/src/learning_thread_in_java/class_thread.java
@@ -2,6 +2,18 @@ package learning_thread_in_java;
 
 public class class_thread {
 	
-	/*sdfsdf*/
+  public static void main(String[] args) throws InterruptedException {
+	
+	  for (int pos = 0; pos < 10; pos ++ ) {
+	  
+		  
+		  /*quero executar esse envio de algo com tempo de parada ou com tempo determinado*/
+		  System.out.println("executando algo como exemplo");
+		  Thread.sleep(100);  //DA UM TEMPO A CADA EXECUÇÃO (RECEBE EM MILI-SEGUNDOS)
+	  }
+	  
+	  
+	  System.out.println("CHEGOU AO FINAL DO TESTE NO FOR");
+}
 
 }
